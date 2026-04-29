e25f3669d4a72219d5d3852818e9825c00788a88
kr0ma-git <w4ynekousuke@gmail.com>
2026-04-29 18:01:33 +0800

    New User Endpoints for Fetching Data

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # CineMatch API v1.0
 
+## Changelog
+
+Initial Commit:
+
 - Connected to MongoDB Atlas Database
 - Created model for the User
 - Added controllers and routes for user-specific server actions
 - Added user registration, login, logout
+
+4/29/2026
+
+- Added new endpoints for user-related queries
--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -46,12 +46,12 @@ const loginUser = async (req, res) => {
         });
 
         if (!user) {
-            return res.status(400).json({ message: "Incorrect username or password" });
+            return res.status(404).json({ message: "Incorrect username or password" });
         }
 
         const isMatching = await user.comparePassword(password);
         if (!isMatching) {
-            return res.status(400).json({ message: "Incorrect username or password" })
+            return res.status(404).json({ message: "Incorrect username or password" })
         }
 
         res.status(200).json({
@@ -63,7 +63,7 @@ const loginUser = async (req, res) => {
             }
         });
     } catch(error) {
-        res.status(500).json({ message: "Internal server error", error: error.message })
+        res.status(500).json({ message: "Internal server error", error: error.message });
     }
 }
 
@@ -83,8 +83,69 @@ const logoutUser = async (req, res) => {
     }
 }
 
+const getUserById = async (req, res) => {
+    try {
+        const { userId } = req.params;
+        const user = await User.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            message: "User found",
+            user,
+        });
+    } catch(error) {
+        res.status(500).json({ message: "Internal server error", error: error.message });
+    }
+}
+
+const getUserByEmail = async (req, res) => {
+    try {
+        const { email } = req.params;
+        const user = await User.findOne({
+            email: email.toLowerCase()
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        res.status(200).json({
+            message: "User found",
+            user,
+        });
+    } catch(error) {
+        res.status(500).json({ message: "Internal server error", error: error.message});
+    }
+}
+
+const getAllUsers = async (req, res) => {
+    try {
+        // Does not include passwords
+        const userData = await User.find().select("-password");
+
+        if (userData.length === 0) {
+            res.status(404).json({ message: "No users in the database" });
+        }
+
+        return res.status(200).json({
+            message: "Users fetched",
+            userData,
+        })
+    } catch(error) {
+        res.status(500).json({ message: "Internal server error", error: error.message});
+    }
+}
+
 export {
+    // POST
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    // GET
+    getUserById,
+    getUserByEmail,
+    getAllUsers,
 }
