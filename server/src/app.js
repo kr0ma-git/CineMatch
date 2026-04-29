a674314511d68bbd85aeee82c540fbc8374d15e1
kr0ma-git <w4ynekousuke@gmail.com>
2026-04-29 22:33:47 +0800

    Added bookmark schema, routes, and controller. Added review schema. Improved documentation

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,12 @@
-import express from "express";
+import express, { application } from "express";
 import userRouter from "./routes/user.route.js";
+import bookmarkRouter from "./routes/bookmark.route.js";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/bookmarks", bookmarkRouter);
 
 export default app;
