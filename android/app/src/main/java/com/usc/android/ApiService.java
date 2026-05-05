65e73c41ddc5a97dd7eacfa7aa124255dfa5f5de
DPTDM <22105026@usc.edu.ph>
2026-05-05 09:52:00 +0800

    Added a landing page

--- android/app/src/main/java/com/usc/android/ApiService.java
+++ android/app/src/main/java/com/usc/android/ApiService.java
@@ -2,7 +2,9 @@ package com.usc.android;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface ApiService {
     @POST("api/v1/users/register")
@@ -10,4 +12,7 @@ public interface ApiService {
 
     @POST("api/v1/users/login")
     Call<RegisterResponse> loginUser(@Body LoginRequest request);
+
+    @GET("api/v1/users/{id}")
+    Call<RegisterResponse.UserData> getUserProfile(@Path("id") String userId);
 }
---
+++ android/app/src/main/java/com/usc/android/LandingActivity.java
@@ -0,0 +1,41 @@
+package com.usc.android;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class LandingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
+
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
+    }
+}
